d4d34406884b9c54457583b99f7bbc45924032d4
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-26 13:30:01 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,21 @@
+function startPortal(){
 
+const name=document.getElementById("studentName").value.trim();
+
+const district=document.getElementById("district").value;
+
+if(name==="" || district===""){
+
+alert("Please Enter Name and Select District");
+
+return;
+
+}
+
+localStorage.setItem("studentName",name);
+
+localStorage.setItem("district",district);
+
+window.location.href="dashboard.html";
+
+}
