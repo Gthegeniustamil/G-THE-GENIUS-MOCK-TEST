8a3efa9a12dc3acad824260a577bd75b607db3d8
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-26 15:43:02 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,24 @@
 // Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBm0H6zoQ-ogjS7buDPqQKIhc29cqXVmew",
-  authDomain: "g-the-genius.firebaseapp.com",
-  projectId: "g-the-genius",
-  storageBucket: "g-the-genius.firebasestorage.app",
-  messagingSenderId: "472076249930",
-  appId: "1:472076249930:web:f96a883b2560e65f866fb3"
+
+ // உங்கள் Firebase config
+
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+const app = initializeApp(firebaseConfig);
+
+
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
