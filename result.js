d84cc164791daa4f4fde1521b1440bcec9b360ce
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-26 14:44:08 +0530

    Display score, percentage, and answer review

--- result.js
+++ result.js
@@ -1,1 +1,177 @@
+// GET DATA FROM LOCAL STORAGE
 
+
+let score = Number(localStorage.getItem("score")) || 0;
+
+let totalQuestions = 
+Number(localStorage.getItem("totalQuestions")) || 0;
+
+
+let testType = 
+localStorage.getItem("testType") || "Daily";
+
+
+
+let questions = 
+JSON.parse(localStorage.getItem("questions")) || [];
+
+
+let userAnswers = 
+JSON.parse(localStorage.getItem("userAnswers")) || [];
+
+
+
+
+
+// SCORE DISPLAY
+
+
+document.getElementById("score").innerHTML =
+
+score + " / " + totalQuestions;
+
+
+
+
+// PERCENTAGE
+
+
+let percentage = 0;
+
+
+if(totalQuestions > 0){
+
+percentage = Math.round(
+(score / totalQuestions) * 100
+);
+
+}
+
+
+document.getElementById("percentage").innerHTML =
+
+percentage + "%";
+
+
+
+
+
+
+// CORRECT WRONG
+
+
+let wrong = totalQuestions - score;
+
+
+
+document.getElementById("correct").innerHTML =
+score;
+
+
+
+document.getElementById("wrong").innerHTML =
+wrong;
+
+
+
+document.getElementById("total").innerHTML =
+totalQuestions;
+
+
+
+
+
+
+
+
+// ANSWER REVIEW
+
+
+let reviewBox = 
+document.getElementById("reviewContainer");
+
+
+
+questions.forEach((q,index)=>{
+
+
+let userAnswer = userAnswers[index];
+
+
+
+let isCorrect =
+userAnswer === q.answer;
+
+
+
+let card = document.createElement("div");
+
+
+card.className="review-card";
+
+
+
+card.innerHTML = `
+
+
+<h3>
+${index+1}. ${q.question}
+</h3>
+
+
+<p>
+
+Your Answer:
+<b>
+${userAnswer !== null && userAnswer !== undefined 
+? q.options[userAnswer] 
+: "Not Answered"}
+</b>
+
+</p>
+
+
+
+<p>
+
+Correct Answer:
+
+<b>
+${q.options[q.answer]}
+</b>
+
+</p>
+
+
+
+<p class="${isCorrect ? "correct":"wrong"}">
+
+${isCorrect 
+? "✅ Correct Answer" 
+: "❌ Wrong Answer"}
+
+</p>
+
+
+
+<p>
+
+📖 Explanation:
+
+<br>
+
+${q.explanation}
+
+</p>
+
+
+
+`;
+
+
+
+reviewBox.appendChild(card);
+
+
+
+});
