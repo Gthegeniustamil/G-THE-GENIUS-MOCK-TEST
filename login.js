af736731cc3579fef72fd33328f85d3f627824fb
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-28 14:21:53 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,31 @@
+import { auth } from "./firebase-config.js";
 
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+document.getElementById("loginBtn").onclick = async function () {
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    if (!email || !password) {
+        alert("Please enter Email & Password");
+        return;
+    }
+
+    try {
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("Login Successful!");
+
+        window.location.href = "dashboard.html";
+
+    } catch (error) {
+
+        alert("Invalid Email or Password");
+
+    }
+
+};
