6309310be86368f1ad67ee3cb058c0d5c366fd9b
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-28 17:12:11 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,100 @@
-console.log("LOGIN JS LOADED");
+import { auth, db } from "./firebase-config.js";
+
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+doc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
 document.getElementById("loginBtn").onclick = async function(){
 
-    alert("Login Button Clicked");
+
+let email =
+document.getElementById("email").value.trim();
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+if(email === "" || password === ""){
+
+alert("Enter Email and Password");
+
+return;
 
 }
+
+
+
+try{
+
+
+let result =
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+
+
+let user = result.user;
+
+
+
+let userData =
+await getDoc(
+doc(db,"users",user.uid)
+);
+
+
+
+if(userData.exists()){
+
+
+localStorage.setItem(
+"studentName",
+userData.data().name
+);
+
+
+localStorage.setItem(
+"district",
+userData.data().district
+);
+
+
+}
+
+
+
+alert("Login Successful 🎉");
+
+
+window.location.href="dashboard.html";
+
+
+}
+
+
+catch(error){
+
+
+alert(
+"Login Failed : "+error.code
+);
+
+
+console.log(error);
+
+
+}
+
+
+};
