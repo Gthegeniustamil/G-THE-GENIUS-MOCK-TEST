921b5321af692a1bcbb6d6c987f3d0c720d34054
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-28 14:27:43 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,31 +1,97 @@
-import { auth } from "./firebase-config.js";
+import { db, auth } from "./firebase-config.js";
 
 import {
 signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-document.getElementById("loginBtn").onclick = async function () {
+import {
+doc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value;
 
-    if (!email || !password) {
-        alert("Please enter Email & Password");
+document.getElementById("loginBtn").onclick = async function(){
+
+    const email =
+    document.getElementById("email").value.trim();
+
+    const password =
+    document.getElementById("password").value;
+
+
+    if(email === "" || password === ""){
+
+        alert("Enter Email and Password");
         return;
+
     }
 
-    try {
 
-        await signInWithEmailAndPassword(auth, email, password);
+    try{
+
+
+        const userCredential =
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+
+        const uid = userCredential.user.uid;
+
+
+        // Get Student Details
+
+        const studentRef =
+        doc(db,"students",uid);
+
+
+        const studentSnap =
+        await getDoc(studentRef);
+
+
+
+        if(studentSnap.exists()){
+
+
+            const data = studentSnap.data();
+
+
+            localStorage.setItem(
+                "studentName",
+                data.name
+            );
+
+
+            localStorage.setItem(
+                "district",
+                data.district
+            );
+
+
+        }
+
+
 
         alert("Login Successful!");
 
-        window.location.href = "dashboard.html";
 
-    } catch (error) {
+        window.location.href =
+        "dashboard.html";
+
+
+
+    }catch(error){
+
 
         alert("Invalid Email or Password");
 
+
+        console.log(error);
+
+
     }
+
 
 };
