9fc432e948128c580d832183da15d488d3500cf7
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-28 15:41:03 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,10 @@
-import { db, auth } from "./firebase-config.js";
+import { auth, db } from "./firebase-config.js";
+
 
 import {
 signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 
 import {
 doc,
@@ -10,88 +12,136 @@ getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
+
+
+
 document.getElementById("loginBtn").onclick = async function(){
 
-    const email =
-    document.getElementById("email").value.trim();
-
-    const password =
-    document.getElementById("password").value;
 
 
-    if(email === "" || password === ""){
-
-        alert("Enter Email and Password");
-        return;
-
-    }
-
-
-    try{
-
-
-        const userCredential =
-        await signInWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
-
-
-        const uid = userCredential.user.uid;
-
-
-        // Get Student Details
-
-        const studentRef =
-        doc(db,"students",uid);
-
-
-        const studentSnap =
-        await getDoc(studentRef);
+let email =
+document.getElementById("email").value.trim();
 
 
 
-        if(studentSnap.exists()){
-
-
-            const data = studentSnap.data();
-
-
-            localStorage.setItem(
-                "studentName",
-                data.name
-            );
-
-
-            localStorage.setItem(
-                "district",
-                data.district
-            );
-
-
-        }
+let password =
+document.getElementById("password").value;
 
 
 
-        alert("Login Successful!");
 
 
-        window.location.href =
-        "dashboard.html";
+if(email==="" || password===""){
+
+
+alert("Enter Email and Password");
+
+
+return;
+
+
+}
 
 
 
-    }catch(error){
 
 
-        alert("Invalid Email or Password");
+
+try{
 
 
-        console.log(error);
+
+const userCredential =
+
+await signInWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
 
 
-    }
+
+
+const user =
+userCredential.user;
+
+
+
+
+
+
+// Get Student Data From Firestore
+
+
+const userDoc =
+
+await getDoc(
+
+doc(db,"users",user.uid)
+
+);
+
+
+
+
+
+if(userDoc.exists()){
+
+
+
+let data =
+userDoc.data();
+
+
+
+
+localStorage.setItem(
+"studentName",
+data.name
+);
+
+
+
+localStorage.setItem(
+"district",
+data.district
+);
+
+
+
+}
+
+
+
+
+
+alert("Login Successful 🎉");
+
+
+
+window.location.href="dashboard.html";
+
+
+
+
+
+
+}
+
+catch(error){
+
+
+alert("Login Failed : "+error.message);
+
+
+
+}
+
+
 
 
 };
