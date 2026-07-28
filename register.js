6e9bfc2fdf45a945fe9ead06962acf9bce8f2f0d
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-28 15:40:17 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,8 +1,10 @@
-import { db, auth } from "./firebase-config.js";
+import { auth, db } from "./firebase-config.js";
+
 
 import {
 createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 
 import {
 doc,
@@ -10,42 +12,141 @@ setDoc,
 serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+
+
+
+
 document.getElementById("registerBtn").onclick = async function(){
 
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const mobile = document.getElementById("mobile").value.trim();
-    const district = document.getElementById("district").value;
-    const password = document.getElementById("password").value;
 
-    if(!name || !email || !mobile || !district || !password){
-        alert("Please fill all fields");
-        return;
-    }
 
-    try{
+let name =
+document.getElementById("name").value.trim();
 
-        const userCredential =
-        await createUserWithEmailAndPassword(auth,email,password);
 
-        await setDoc(doc(db,"students",userCredential.user.uid),{
 
-            name:name,
-            email:email,
-            mobile:mobile,
-            district:district,
-            createdAt:serverTimestamp()
+let email =
+document.getElementById("email").value.trim();
 
-        });
 
-        alert("Registration Successful!");
 
-        window.location.href="index.html";
+let password =
+document.getElementById("password").value;
 
-    }catch(error){
 
-        alert(error.message);
 
-    }
+let district =
+document.getElementById("district").value;
+
+
+
+
+
+
+if(name==="" || email==="" || password==="" || district===""){
+
+
+alert("Please Fill All Details");
+
+
+return;
+
+
+}
+
+
+
+
+
+
+try{
+
+
+
+// Create Firebase Account
+
+
+const userCredential =
+
+await createUserWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+
+
+const user =
+userCredential.user;
+
+
+
+
+
+
+
+// Save User Data
+
+
+await setDoc(
+
+doc(db,"users",user.uid),
+
+{
+
+
+name:name,
+
+
+email:email,
+
+
+district:district,
+
+
+createdAt:serverTimestamp()
+
+
+}
+
+
+
+);
+
+
+
+
+
+
+
+alert("Registration Successful 🎉");
+
+
+
+window.location.href="index.html";
+
+
+
+
+
+}
+
+catch(error){
+
+
+
+alert(error.message);
+
+
+
+}
+
+
+
+
 
 };
